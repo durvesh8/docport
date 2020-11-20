d276e4f84c4f4432357df2bf6a0b486d0d194070
Durvesh Malpure <62510468+durvesh8@users.noreply.github.com>
2020-11-20 22:26:38 +0530

    Added dummy data for patientdata

--- lib/screens/PatientData/components/body.dart
+++ lib/screens/PatientData/components/body.dart
@@ -5,7 +5,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child:Text("Patient Name:\nPast records:\nAnd a bunch of other data")
     );
   }
 }
